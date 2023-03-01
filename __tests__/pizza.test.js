b04b5cf58115d7366ec9968fbad41a5ef74fb917
Tyler Adams <skipmarcel@gmail.com>
2023-02-28 20:21:05 -0800

    update test with each size and topping numbers

--- __tests__/pizza.test.js
+++ __tests__/pizza.test.js
@@ -16,12 +16,49 @@ describe("Pizza", () => {
     expect(pizza.toppings).toEqual(1);
   });
 
-  test("should correctly return a base price", () => {
+  test("should correctly return a base price for extra-large pizza", () => {
+    pizza = new Pizza("extra-large", 1);
+    expect(pizza.getBasePrice()).toEqual(10);
+  });
+
+  test("should correctly return a base price for large pizza", () => {
+    pizza = new Pizza("large", 1);
     expect(pizza.getBasePrice()).toEqual(8);
   });
 
-  test("should correctly return a topping price", () => {
+  test("should correctly return a base price for extra-large pizza", () => {
+    pizza = new Pizza("medium", 1);
+    expect(pizza.getBasePrice()).toEqual(6);
+  });
+
+  test("should correctly return a base price for extra-large pizza", () => {
+    pizza = new Pizza("small", 1);
+    expect(pizza.getBasePrice()).toEqual(4);
+  });
+
+  test("should correctly return a topping price for 4 topping", () => {
+    pizza = new Pizza("small", 4);
+    expect(pizza.getToppingsPrice()).toEqual(4);
+  });
+
+  test("should correctly return a topping price for 4 topping", () => {
+    pizza = new Pizza("small", 3);
+    expect(pizza.getToppingsPrice()).toEqual(3);
+  });
+
+  test("should correctly return a topping price for 4 topping", () => {
+    pizza = new Pizza("small", 2);
+    expect(pizza.getToppingsPrice()).toEqual(2);
+  });
+
+  test("should correctly return a topping price for 4 topping", () => {
+    pizza = new Pizza("small", 1);
     expect(pizza.getToppingsPrice()).toEqual(1);
+  });
+
+  test("should correctly return a topping price for 4 topping", () => {
+    pizza = new Pizza("small", 0);
+    expect(pizza.getToppingsPrice()).toEqual(0);
   });
 
   test("should correctly calculate a price", () => {
